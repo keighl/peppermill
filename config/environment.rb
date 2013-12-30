4b3106ff3059ff9b376aa8fb0945a0d1b3ee3b9e
Kyle Truscott <kyle@clambakeapp.com>
2013-12-29 23:30:10 -0500

    Another regex update

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-EMAIL_REGEX = /^[A-Za-z0-9][A-Za-z0-9\-\.\+\_]+?([A-Za-z0-9]+)@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i
+EMAIL_REGEX =/^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
 
 TODO_PRIORITIES = {
   low:  0,
